90ab895c45b5f7aef5206faf4f8dca1df913e03b
muhammadfajarramadhan <79896679+muhammadfajarramadhan@users.noreply.github.com>
2021-04-10 19:30:41 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+$servername = "localhost";
+$database = "id16524346_zakat_db";
+$username = "id16524346_root";
+$password = "4zG<b<?PDcPok^N0";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $database);
+?>
